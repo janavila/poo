89c9cdea57a30e55faece243f7d6e90a4244e15a
janavila <jansen-avila@hotmail.com>
2019-11-11 22:58:58 -0200

    trabalho 1(canetas)

---
+++ t1/Lista/src/lista/caneta.java
@@ -0,0 +1,20 @@
+package lista;
+
+public class caneta {
+    
+    private String cor;
+    
+    public caneta(String cor) {
+        this.cor = cor;
+    }
+    
+    public String getCor() {
+        return cor;
+    }
+    
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+    
+    
+}
